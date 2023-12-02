a12c930ce34fbdbbb7ebfbba2d398189a3182ff3
madad <alexander.laputko@ya.ru>
2023-12-02 16:37:18 +0300

    Keep the state of activity. The Stopwatch application is completed.

--- app/src/main/java/hfa/madad/stopwatch/MainActivity.kt
+++ app/src/main/java/hfa/madad/stopwatch/MainActivity.kt
@@ -7,14 +7,31 @@ import android.widget.Button
 import android.widget.Chronometer
 
 class MainActivity : AppCompatActivity() {
+
     lateinit var stopwatch: Chronometer
     var running = false
     var offset = 0L
+
+    //Добавление строк для ключей, используемых с Bundle
+    val OFFSET_KEY = "offset"
+    val RUNNING_KEY = "running"
+    val BASE_KEY = "base"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         stopwatch = findViewById(R.id.stopwatch)
+
+        //Восстановление предыдущего состояния
+        if (savedInstanceState != null) {
+            offset = savedInstanceState.getLong(OFFSET_KEY)
+            running = savedInstanceState.getBoolean(RUNNING_KEY)
+            if (running) {
+                stopwatch.base = savedInstanceState.getLong(BASE_KEY)
+                stopwatch.start()
+            } else setBaseTime()
+        }
 
         //Кнопка start запускает секундомер, если он не работал
         val startButton = findViewById<Button>(R.id.start)
@@ -42,6 +59,30 @@ class MainActivity : AppCompatActivity() {
             offset = 0
             setBaseTime()
         }
+    }
+
+    override fun onPause() {
+        super.onPause()
+        if (running) {
+            saveOffset()
+            stopwatch.stop()
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        if (running) {
+            setBaseTime()
+            stopwatch.start()
+            offset = 0
+        }
+    }
+
+    override fun onSaveInstanceState(savedInstanceState: Bundle) {
+        savedInstanceState.putLong(OFFSET_KEY, offset)
+        savedInstanceState.putBoolean(RUNNING_KEY, running)
+        savedInstanceState.putLong(BASE_KEY, stopwatch.base)
+        super.onSaveInstanceState(savedInstanceState)
     }
 
     //Обновляет время stopwatch.base
